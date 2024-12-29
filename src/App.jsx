dafea384761a996cfd7a30f8baef639b5421c511
Bilal Akram <work.bilalakram@gmail.com>
2024-12-29 09:11:36 +0100

    Add test function again

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
 import { EXAMPLES } from "./data.js";
 
+function test() {
+  console.log("test");
+}
+
 function App() {
   const [selectedTopic, setSelectedTopic] = useState();
 
